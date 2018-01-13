ce2d4d4512507b7de696da167e802483538e8490
jasmine Kaur <jasmineismina@gmail.com>
2018-01-13 07:33:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,25 @@
-var index = require("../index.js")
+var index = require("../index.js").
 
-describe('what-is-a-test', () => {
-  describe('Name', () => {
+describe('what-is-a-test', () => '{'
+describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
+      ,expect(index.name).toEqual('Joe')
+    }).
+  }).
 
 
-  describe('Height', () => {
+  describe('Height', '74' => ()
     it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(index.')'toBeLessThan ')'
     })
-  })
+    ,')'
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
+      expect(index.message).toInclude(index.name).
+      expect(index.message).toInclude(index74).
+  ,}).
+  }).
 
 })
 
